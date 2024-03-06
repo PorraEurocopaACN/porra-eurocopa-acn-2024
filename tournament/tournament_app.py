cd5c68e393a4551ada9d5cb34eedbcc497a5702a
r.baranda.castrillo <r.baranda.castrillo@accenture.com>
2024-03-06 13:10:09 +0100

    Display a message if submission wants to be done without populating user_id

--- tournament/tournament_app.py
+++ tournament/tournament_app.py
@@ -45,61 +45,47 @@ class MyApp:
         st.subheader("Stage Groups ⚽")
 
     def get_unique_group_names(self):
-        self.data_model.cursor.execute("SELECT DISTINCT group_name FROM tbl_actual_groups;")
+        self.data_model.cursor.execute("SELECT DISTINCT group_name FROM tbl_group_games;")
         return [row[0] for row in self.data_model.cursor.fetchall()]
 
     def select_group(self):
         group_names = self.get_unique_group_names()
         # Create a selectbox widget to choose group names
         selected_group = st.selectbox("Select a group name:", group_names)
-        query = f"SELECT * FROM tbl_actual_groups WHERE group_name = '{selected_group}';"
+        query = f"SELECT * FROM tbl_group_games WHERE group_name = '{selected_group}';"
         self.data_model.cursor.execute(query)
         results = self.data_model.cursor.fetchall()
         column_names = [description[0] for description in self.data_model.cursor.description]
         df = pd.DataFrame(results, columns=column_names)
-        games_df = self.generate_game_combinations(df, selected_group)
-        st.table(games_df)
-        return games_df
+        st.table(df)
+        return df
 
-    def generate_game_combinations(self, df, selected_group):
-        game_combinations = []
-        countries = df[df['group_name'] == selected_group]['country'].tolist()
-        group_combinations = list(combinations(countries, 2))
-        game_combinations.extend(group_combinations)
-        games_df = pd.DataFrame(game_combinations, columns=['Local', 'Visitor'])
-        return games_df
 
     def bet_games(self, games_df):
         with st.form(key='game_result_form'):
             # Define a dictionary to map game IDs to local and visitor teams
             game_teams_map = {f"{local}-{visitor}": (local, visitor) for local, visitor in
-                              games_df[['Local', 'Visitor']].values}
+                              games_df[['local', 'visitor']].values}
             # Select game
             selected_game_id = st.selectbox("Select a game:", list(game_teams_map.keys()))
-
             # Get local and visitor teams based on selected game ID
             local, visitor = game_teams_map[selected_game_id]
-
             # Set result
             local_score = st.number_input("Local Score", min_value=0, step=1)
             visitor_score = st.number_input("Visitor Score", min_value=0, step=1)
-
-            print(f"local: {local}")
-            print(f"visitor: {visitor}")
-            print(f"local_score: {local_score}")
-            print(f"visitor_score: {visitor_score}")
-
             # Submit button
             submitted = st.form_submit_button("Submit")
 
         # If form is submitted, update the database with the result
         if submitted:
-            # Update the database with the result
-            self.data_model.cursor.execute(f"INSERT INTO tbl_user_predictions values (:user_id, :local, :visitor, :local_goals, :visitor_goals)",
-                           (self.user_id, local, visitor, local_score, visitor_score))
-            self.data_model.connect.commit()
-            st.success("Result updated successfully!")
-
+            if self.user_id is not '':
+                # Update the database with the result
+                self.data_model.cursor.execute(f"INSERT INTO tbl_user_predictions values (:user_id, :local, :visitor, :local_goals, :visitor_goals)",
+                               (self.user_id, local, visitor, local_score, visitor_score))
+                self.data_model.connect.commit()
+                st.success("Result updated successfully! ✅")
+            else:
+                st.error(f"You need to populate your user_id if you want to submit a prediction. 🌚")
         self.data_model.select_all_from_tables()
 
     def display_all_bets(self):
