eba185ab56bed1665a8fb188dbac7b01cf0e36b7
r.baranda.castrillo <r.baranda.castrillo@accenture.com>
2024-03-06 16:43:45 +0100

    Refactor

--- tournament/data_model.py
+++ tournament/data_model.py
@@ -3,24 +3,38 @@ import os
 import json
 from pathlib import Path
 import pandas as pd
-import streamlit as st
 
 
 class DataModel:
-    def __init__(self, populate_initial_tables=False):
-        self.connect = sqlite3.connect("pwd.db")
-        self.cursor = self.connect.cursor()
+    """
+    A class representing the data model for managing Eurocup 2024 predictions.
+    """
+
+    def __init__(self, populate_initial_tables: bool = False) -> None:
+        """
+        Initializes the DataModel class.
+
+        Parameters:
+        - populate_initial_tables (bool): Whether to populate initial tables upon initialization.
+        """
+        self.connect: sqlite3.Connection = sqlite3.connect("pwd.db")
+        self.cursor: sqlite3.Cursor = self.connect.cursor()
         self.create_tables()
         self.select_all_from_tables()
         if populate_initial_tables:
             self.populate_initial_tables()
 
     def create_tables(self):
+        """
+        Creates necessary tables in the database if they do not exist.
+        """
         # Get the path to the JSON file
-        json_file_path = os.path.join(os.path.dirname(__file__), "tables_definition.json")
+        json_file_path = os.path.join(
+            os.path.dirname(__file__), "tables_definition.json"
+        )
 
         # Load the JSON file
-        with open(json_file_path, 'r') as f:
+        with open(json_file_path, "r") as f:
             table_definitions = json.load(f)["tables"]
 
         # Iterate over each table definition and create the tables
@@ -29,19 +43,49 @@ class DataModel:
             columns = table_definition["columns"]
             primary_key = table_definition["primary_key"]
 
-            column_definitions = ', '.join(
-                [f"{column['name']} {column['type']} {column['constraints']}" for column in columns])
-            primary_key_definition = f", PRIMARY KEY ({', '.join(primary_key)})" if primary_key else ""
+            column_definitions = ", ".join(
+                [
+                    f"{column['name']} {column['type']} {column['constraints']}"
+                    for column in columns
+                ]
+            )
+            primary_key_definition = (
+                f", PRIMARY KEY ({', '.join(primary_key)})" if primary_key else ""
+            )
             create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ({column_definitions}{primary_key_definition});"
             self.cursor.execute(create_table_sql)
         self.connect.commit()
 
+    def populate_initial_tables(self):
+        """
+        Populates initial tables in the database upon initialization.
+        """
+        self.populate_groups_table()
+
+    def populate_groups_table(self):
+        """
+        Populates the 'tbl_group_games' table with data from an Excel file uploaded within data folder.
+        """
+        games_calendar_path = os.path.join(
+            Path(__file__).parent.parent, "data", "eurocup_games_calendar.xlsx"
+        )
+        df = pd.read_excel(games_calendar_path, sheet_name="group_stage")
+        df.to_sql(
+            name="tbl_group_games", con=self.connect, if_exists="replace", index=False
+        )
+        self.connect.commit()
+
     def select_all_from_tables(self):
+        """
+        Retrieves and prints all data from all tables in the database.
+        """
         # Get the path to the JSON file
-        json_file_path = os.path.join(os.path.dirname(__file__), "tables_definition.json")
+        json_file_path = os.path.join(
+            os.path.dirname(__file__), "tables_definition.json"
+        )
 
         # Load the JSON file
-        with open(json_file_path, 'r') as f:
+        with open(json_file_path, "r") as f:
             table_definitions = json.load(f)["tables"]
 
         # Iterate over each table definition and execute SELECT * query
@@ -53,12 +97,3 @@ class DataModel:
             print(f"Data from table {table_name}:")
             for row in rows:
                 print(row)
-
-    def populate_groups_table(self):
-        games_calendar_path = os.path.join(Path(__file__).parent.parent, "data", "eurocup_games_calendar.xlsx")
-        df = pd.read_excel(games_calendar_path, sheet_name='group_stage')
-        df.to_sql(name='tbl_group_games', con=self.connect, if_exists='replace', index=False)
-        self.connect.commit()
-
-    def populate_initial_tables(self):
-        self.populate_groups_table()
--- tournament/tournament_app.py
+++ tournament/tournament_app.py
@@ -3,71 +3,122 @@ import webbrowser
 import sqlite3
 from tournament.data_model import DataModel
 import pandas as pd
-from itertools import combinations
+from typing import Optional
 
 
 class MyApp:
+    """
+    A class representing the Streamlit application for managing Eurocup 2024 predictions.
+    """
+
     def __init__(self):
-        self.title = "SC&OA Eurocup 2024"
-        self.stage_groups = "Stage Groups"
-        self.user_id = None
-        self.page_icon = "🚀"
-        self.data_model = DataModel()
+        """
+        Initializes the MyApp class.
+        """
+        self.title: str = "SC&OA Eurocup 2024"
+        self.stage_groups: str = "Stage Groups"
+        self.user_id: Optional[str] = None
+        self.page_icon: str = "🚀"
+        self.data_model: DataModel = DataModel()
 
-    def run(self):
-        self.set_initial_page()
-        radio_option = self.set_basic_properties()
-
+    def run(self) -> None:
+        """
+        Runs the Streamlit application.
+        """
+        self.set_initial_page_interface()
+        radio_option = self.set_basic_properties_interface()
         if radio_option == "Predictions":
-            self.set_stage_groups_bet()
-            # Text input for user ID
-            self.user_id = st.text_input("Enter User ID:")
-            games_df = self.select_group()
-            self.bet_games(games_df)
-
+            self.create_predictions_tab()
         if radio_option == "Results":
             st.subheader("Results 🗝️")
             self.display_all_bets()
-
         if radio_option == "GenAI":
             st.subheader("GenAI 🔄")
 
-    def set_basic_properties(self):
+    def create_predictions_tab(self) -> None:
+        """
+        Creates the predictions tab.
+        """
+        self.set_stage_groups_interface()
+        # Text input for user ID
+        self.user_id = st.text_input("Enter User ID:")
+        games_df = self.select_group()
+        self.predict_games(games_df)
+
+    def set_basic_properties_interface(self) -> str:
+        """
+        Sets the basic properties interface.
+
+        Returns:
+        - str: The selected radio option.
+        """
         st.title("Eurocup 2024 ⚽")
-        radio_option = st.sidebar.radio("Menu", options=["Predictions", "Results", "Gen AI"])
+        radio_option = st.sidebar.radio(
+            "Menu", options=["Predictions", "Results", "Gen AI"]
+        )
         return radio_option
 
-
-    def set_initial_page(self):
+    def set_initial_page_interface(self) -> None:
+        """
+        Sets the initial page interface.
+        """
         st.set_page_config(page_title=self.title, page_icon=self.page_icon)
 
-    def set_stage_groups_bet(self):
+    def set_stage_groups_interface(self) -> None:
+        """
+        Sets the stage groups interface.
+        """
         st.subheader("Stage Groups ⚽")
 
-    def get_unique_group_names(self):
-        self.data_model.cursor.execute("SELECT DISTINCT group_name FROM tbl_group_games;")
+    def get_unique_group_names(self) -> list[str]:
+        """
+        Retrieves unique group names from the database.
+
+        Returns:
+        - list[str]: A list of unique group names.
+        """
+        self.data_model.cursor.execute(
+            "SELECT DISTINCT group_name FROM tbl_group_games;"
+        )
         return [row[0] for row in self.data_model.cursor.fetchall()]
 
-    def select_group(self):
+    def select_group(self) -> pd.DataFrame:
+        """
+        Selects a group from the database.
+
+        Returns:
+        - pd.DataFrame: DataFrame containing the selected group data.
+        """
         group_names = self.get_unique_group_names()
         # Create a selectbox widget to choose group names
         selected_group = st.selectbox("Select a group name:", group_names)
         query = f"SELECT * FROM tbl_group_games WHERE group_name = '{selected_group}';"
         self.data_model.cursor.execute(query)
         results = self.data_model.cursor.fetchall()
-        column_names = [description[0] for description in self.data_model.cursor.description]
+        column_names = [
+            description[0] for description in self.data_model.cursor.description
+        ]
         df = pd.DataFrame(results, columns=column_names)
         st.table(df)
         return df
 
+    def predict_games(self, games_df: pd.DataFrame) -> None:
+        """
+        Predicts the outcome of games.
 
-    def bet_games(self, games_df):
-        with st.form(key='game_result_form'):
+        Parameters:
+        - games_df (pd.DataFrame): DataFrame containing game data.
+        """
+        with st.form(key="game_result_form"):
             # Define a dictionary to map game IDs to local and visitor teams
-            game_teams_map = {f"{local}-{visitor}": (local, visitor) for local, visitor in
-                              games_df[['local', 'visitor']].values}
+            game_teams_map = {
+                f"{local}-{visitor}": (local, visitor)
+                for local, visitor in games_df[["local", "visitor"]].values
+            }
             # Select game
-            selected_game_id = st.selectbox("Select a game:", list(game_teams_map.keys()))
+            selected_game_id = st.selectbox(
+                "Select a game:", list(game_teams_map.keys())
+            )
             # Get local and visitor teams based on selected game ID
             local, visitor = game_teams_map[selected_game_id]
             # Set result
@@ -78,17 +129,24 @@ class MyApp:
 
         # If form is submitted, update the database with the result
         if submitted:
-            if self.user_id is not '':
+            if self.user_id != "":
                 # Update the database with the result
-                self.data_model.cursor.execute(f"INSERT INTO tbl_user_predictions values (:user_id, :local, :visitor, :local_goals, :visitor_goals)",
-                               (self.user_id, local, visitor, local_score, visitor_score))
+                self.data_model.cursor.execute(
+                    f"INSERT INTO tbl_user_predictions values (:user_id, :local, :visitor, :local_goals, :visitor_goals)",
+                    (self.user_id, local, visitor, local_score, visitor_score),
+                )
                 self.data_model.connect.commit()
                 st.success("Result updated successfully! ✅")
             else:
-                st.error(f"You need to populate your user_id if you want to submit a prediction. 🌚")
+                st.error(
+                    f"You need to populate your user_id if you want to submit a prediction. 🌚"
+                )
         self.data_model.select_all_from_tables()
 
-    def display_all_bets(self):
+    def display_all_bets(self) -> None:
+        """
+        Displays all bets made by users.
+        """
         # Execute SQL query to retrieve all records from tbl_user_predictions
         self.data_model.cursor.execute("SELECT * FROM tbl_user_predictions")
         results = self.data_model.cursor.fetchall()
@@ -96,10 +154,9 @@ class MyApp:
         # Display the results in a table
         if results:
             # Get column names
-            column_names = [description[0] for description in self.data_model.cursor.description]
-
-            # Create a DataFrame from the results
-            import pandas as pd
+            column_names = [
+                description[0] for description in self.data_model.cursor.description
+            ]
             df = pd.DataFrame(results, columns=column_names)
 
             # Display the DataFrame as a table
@@ -107,4 +164,3 @@ class MyApp:
             st.table(df)
         else:
             st.write("No bets found.")
-
